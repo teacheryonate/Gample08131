482496ee523d8500ce3ada693c94249dc187fefb
tj705 <a@a.com>
2023-08-14 09:21:41 +0900

    first work

---
+++ src/main/java/com/example/demo/Gample08131Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gample08131Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gample08131Application.class, args);
+	}
+
+}
